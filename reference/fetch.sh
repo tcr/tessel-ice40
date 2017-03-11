266a8b8662ded5964073ac9296fff629ac93ea75
Tim Ryan <id@timryan.org>
2017-03-10 20:47:22 -0500

    Fetch.

--- reference/fetch.sh
+++ reference/fetch.sh
@@ -3,3 +3,4 @@
 wget https://www.latticesemi.com/~/media/LatticeSemi/Documents/ApplicationNotes/IK/iCE40ProgrammingandConfiguration.pdf -O ice40-programming.pdf
 wget http://www.latticesemi.com/~/media/LatticeSemi/Documents/DataSheets/iCE/iCE40LPHXFamilyDataSheet.pdf -O ice40-datasheet.pdf
 wget http://www.mouser.com/ds/2/368/Si501-2-3-276739.pdf -O si501.pdf
+wget http://www.ti.com/lit/ds/symlink/tps62207.pdf -O tps62207.pdf
